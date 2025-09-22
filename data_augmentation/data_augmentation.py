efbf169f4fff3ceba035433f1166566391e16880
moatazhamza194 <moatazhamza194@gmail.com>
2025-09-22 15:35:52 +0300

    now the dataset file can be passed as input when running

--- data_augmentation/data_augmentation.py
+++ data_augmentation/data_augmentation.py
@@ -1,17 +1,16 @@
 import pandas as pd
 import json
-from sklearn.model_selection import train_test_split
+import sys
+
 
 # import the augmentation functions
 from gender_swap import augment_with_gender_swap
 from gender_mask import augment_with_gender_mask
 
-# import the dataset
-data = pd.read_csv("../subdataset/subdataset.csv")
+# input training dataset
+input_file = sys.argv[1]
+data = pd.read_csv(input_file)
 
-# splitting
-train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
-train_data = train_data.drop(columns=["gender"]) # we dont need it for training
 
 # import the dictionary
 with open("../gendered_word_pairs/gendered_word_pairs.json", "r", encoding="utf-8") as f:
@@ -25,15 +24,20 @@ for k, v in list(gendered_word_pairs.items()):
 genderTerms = sorted({str(k).lower() for k in gendered_word_pairs.keys()} |
                {str(v).lower() for v in gendered_word_pairs.values()})
 
-gender_swap_data = augment_with_gender_swap(train_data, gendered_word_pairs)
-gender_mask_data = augment_with_gender_mask(train_data, gendered_word_pairs)
+gender_swap_data = augment_with_gender_swap(data, gendered_word_pairs)
+gender_mask_data = augment_with_gender_mask(data, gendered_word_pairs)
 
-#print(train_data.head())
+#print(data.head())
 #print(gender_swap_data.head())
 #print(gender_mask_data.head())
 
+
+
 # add the gender swapped rows to the original data
-gender_swap_data = pd.concat([train_data, gender_swap_data], ignore_index=True)
+gender_swap_data = pd.concat([data, gender_swap_data], ignore_index=True)
+
 
 gender_swap_data.to_csv("gender_swap_data.csv", index=False)
 gender_mask_data.to_csv("gender_mask_data.csv", index=False)
+
+
